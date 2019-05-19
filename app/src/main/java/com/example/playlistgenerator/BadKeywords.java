f6f8f3750019b045101d5a90c8663991627e6a0e
Anna Thomas <anna.tina.thomas@gmail.com>
2019-05-19 13:28:15 -0700

    Updated UI
    Linked separate activites, back button works, buttons work (except for play my playlists and view masterplaylist), playlist result should be displayed in an acitivity with a nested scrolling box

--- app/src/main/java/com/example/playlistgenerator/BadKeywords.java
+++ app/src/main/java/com/example/playlistgenerator/BadKeywords.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class GoodKeywords extends AppCompatActivity {
+public class BadKeywords extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_good_keywords);
+        setContentView(R.layout.activity_bad_keywords);
     }
 
     public void onClick(View view) {
         Intent onClick = new Intent(this, MyPlaylists.class);
-        startActivity(onClick);      
+        startActivity(onClick);
     }
 }
--- app/src/main/java/com/example/playlistgenerator/GoodBad.java
+++ app/src/main/java/com/example/playlistgenerator/GoodBad.java
@@ -2,13 +2,43 @@ package com.example.playlistgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GoodBad extends AppCompatActivity {
+    private TextView goodBad;   // a text view for debugging messages
+    private Button good;  // view for the button to play the playlist
+    private Button bad;  // view for the button to play the playlist
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_good_bad);
+        // Initialize views/buttons
+        goodBad = findViewById(R.id.textViewQuestion);
+          good = findViewById(R.id.buttonGood);
+          bad = findViewById(R.id.buttonBad);
+
     }
+
+    public void onGoodClick(View view) {
+        Intent onGoodClick = new Intent(this, GoodKeywords.class);
+        startActivity(onGoodClick);
+    }
+
+    public void onBadClick(View view) {
+        Intent onBadClick = new Intent(this, BadKeywords.class);
+        startActivity(onBadClick);
+    }
+
+//    public void onBadClick(View view) {
+//        Intent onBadClick = new Intent(this, GoodKeywords.class);
+//        startActivity(onBadClick);
+//    }
+
 }
